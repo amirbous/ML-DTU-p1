52be28832925cdcb319b2253cd591a707dd94835
amirbous <a.amirbouslama@gmail.com>
2023-10-31 14:09:05 +0100

    finished standard linear regression, still no regularization parameter

--- project2_b.R
+++ project2_b.R
@@ -22,7 +22,7 @@ colnames(data)
 # (Age, Height) -> (Weight)
 
 tmp_x <- data[, (names(data) %in% c("Age", "Height",
-                                "family_history_with_overweight", "MTRANS"))]
+                                    "family_history_with_overweight", "MTRANS"))]
 tmp_x <- as.matrix(tmp_x)
 X <- cbind(rep(1, length(data$Age)), tmp_x)
 y <- as.numeric(data$Weight)
@@ -48,19 +48,23 @@ T <- M + 1
 
 # Initialize variables
 
-temp <- rep(NA, M * KK)
 #all_w <- array(temp, c(M, T, KK))
 
 
 
 ws_final <- matrix(rep(NA, times = K * M), nrow = M)
+error_train_final <- c()
+error_test_final <- c()
 Error_train_rlr <- rep(NA, K)
 Error_test_rlr <- rep(NA, K)
 w_noreg <- matrix(rep(NA, times = M * K), nrow = M)
-mu <- matrix(rep(NA, times = (M - 1) * K), nrow = K)
-sigma <- matrix(rep(NA, times = (M - 1) * K), nrow = K)
-Error_train <- rep(NA, K)
-Error_test <- rep(NA, K)
+
+
+y_validate <- c()
+y_train_inner <- c()
+X_train_inner <- matrix(rep(NA, times = K * M), nrow = M)
+
+X_validate <- matrix(rep(NA, times = K * M), nrow = M)
 
 
 ## constructing the model:
@@ -70,7 +74,7 @@ Error_test <- rep(NA, K)
 
 for (k in 1:K) {
   paste("Crossvalidation fold ", k, "/", K, sep = "")
-  
+  MSE_min <- 0
   # Extract the training and test set
   X_train <- X[CV$which != k, ]
   y_train <- y[CV$which != k]
@@ -78,6 +82,8 @@ for (k in 1:K) {
   y_test <- y[CV$which == k]
   CV$TrainSize[k] <- length(y_train)
   CV$TestSize[k] <- length(y_test)
+  
+  
   
   # Use 10-fold cross-validation to get the best liner regression model
   KK <- 10
@@ -90,19 +96,19 @@ for (k in 1:K) {
   ## find omega vector that gives the min 
   
   X_train_default <- X_train[CV2$which != 1,]
-  
   y_train_default <- y_train[CV2$which != 1]
   X_validate_default <- X_train[CV2$which == 1, ]
   y_validate_default <- y_train[CV2$which == 1]
   
   Xty_default <- t(X_train_default) %*% y_train_default
   XtX_default <- t(X_train_default) %*% X_train_default
-
+  
   w_optimal <- solve(XtX_default) %*% Xty_default
   error_vector <- c()
-  for (kx in 1:(length(X_validate_default[, 1]) - 1)){
-    error_vector[kx] = y_validate[kx] - dot(w_optimal, X_validate_default[kx, ])
-
+  for (kx in 1:(length(y_validate_default))){
+    
+    error_vector[kx] = y_validate_default[kx] - dot(w_optimal, X_validate_default[kx, ])
+    
   }
   
   MSE_min <- (sum(error_vector^2))/length(y_validate_default)
@@ -113,13 +119,13 @@ for (k in 1:K) {
   for (kk in 1:KK) {
     
     ## in this inner loop, the goal is 
-    
+    y_validate <- y_train[CV2$which == kk]
     X_train_inner <- X_train[CV2$which != kk, ]
     y_train_inner <- y_train[CV2$which != kk]
     X_validate <- X_train[CV2$which == kk, ]
-    y_validate <- y_train[CV2$which == kk]
     
-    CV2$TrainSize[kk] <- length(y_train)
+    
+    CV2$TrainSize[kk] <- length(y_train_inner)
     CV2$ValidateSize[kk] <- length(y_validate)
     
     Xty_inner <- t(X_train_inner) %*% y_train_inner
@@ -127,79 +133,70 @@ for (k in 1:K) {
     
     w_curr <- solve(XtX_inner) %*% Xty_inner
     error_curr <- c()
-    for (kx in 1:(length(X_validate[, 1]) - 1)){
+    for (kx in 1:(length(X_validate[, 1]))){
       error_curr <- append(error_curr, y_validate[kx] - dot(w_curr, X_validate[kx, ]))
     }
-    print(error_curr)
     MSE_curr <- (sum(error_curr^2))/length(y_validate)
-
+    
     if (MSE_curr <= MSE_min) {
-        MSE_min <- MSE_curr
-        w_optimal <- w_curr
+      MSE_min <- MSE_curr
+      w_optimal <- w_curr
     }
   }
   ws_final[, k] <- w_optimal 
+  error_train_final[k] <- MSE_min
+  error_test_final[k] <- sum((y_test - X_test %*% ws_final[, k])^2)/length(y_test)
   
-
+  
+  
   
   # Assuming your data is already standardized and scaled, you don't need to standardize it again.
   # Simply copy the subsets.
   
   # Estimate w for the optimal value of lambda
-  Xty <- t(X_train) %*% y_train
-  XtX <- t(X_train) %*% X_train
-
   
-  w_rlr[, k] <- solve(XtX) %*% Xty
   
-  # evaluate training and test error performance for optimal selected value of lambda
-  Error_train_rlr[k] <- sum((y_train - X_train %*% w_rlr[, k])^2)
-  Error_test_rlr[k] <- sum((y_test - X_test %*% w_rlr[, k])^2)
+  
+  
   
   # Compute squared error without regularization
   # Adds a small value to diagonal to avoid a singular matrix
-  w_noreg[, k] <- solve(XtX + (diag(M) * 1e-10)) %*% Xty
-  Error_train[k] <- sum((y_train - X_train %*% w_noreg[, k])^2)
-  Error_test[k] <- sum((y_test - X_test %*% w_noreg[, k])^2)
+  #w_noreg[, k] <- solve(XtX + (diag(M) * 1e-10)) %*% Xty
   
   # Compute squared error without using the input data at all
   #Error_train_nofeatures[k] <- sum((y_train - mean(y_train))^2)
   #Error_test_nofeatures[k] <- sum((y_test - mean(y_train))^2)
   
-  if (k == K) {
-    dev.new()
-    # Display result for cross-validation fold
-    w_mean <- apply(w, c(1, 2), mean)
-    
-    # Plot weights as a function of the regularization strength (not offset)
-    par(mfrow = c(1, 2))
-    par(cex.main = 1.5) # Define size of title
-    par(cex.lab = 1) # Define size of axis labels
-    par(cex.axis = 1) # Define size of axis labels
-    par(mar = c(5, 4, 3, 1) + .1) # Increase margin size to allow for larger axis labels
-
+  #if (k == K) {
+  # dev.new()
+  # Display result for cross-validation fold
+  #w_mean <- apply(w, c(1, 2), mean)
   
-    
+  # Plot weights as a function of the regularization strength (not offset)
+  #par(mfrow = c(1, 2))
+  #par(cex.main = 1.5) # Define size of title
+  #par(cex.lab = 1) # Define size of axis labels
+  #par(cex.axis = 1) # Define size of axis labels
+  #par(mar = c(5, 4, 3, 1) + .1) # Increase margin size to allow for larger axis labels
+  
+  
+  
+  # }
+  
+  writeLines("Weights in last fold :")
+  for (m in 1:M) {
+    writeLines(paste(attributeNames[m], ws_final[m, k]))
   }
+  
+  
 }
+
+plot(c(1:10), error_train_final, type = "o", pch = 19,
+     ylim = c(0,1), col = "red", xlab = "fold number")
+lines(c(1:10), error_test_final, type = "o", pch = 19, col = "blue")
 
 # Display Results
-writeLines("Linear regression without feature selection:")
-writeLines(paste("- Training error: ", sum(Error_train) / sum(CV$TrainSize)))
-writeLines(paste("- Test error", sum(Error_test) / sum(CV$TestSize)))
-writeLines(paste("- R^2 train:     %8.2f\n", (sum(Error_train_nofeatures) - sum(Error_train)) / sum(Error_train_nofeatures)))
-writeLines(paste("- R^2 test:     %8.2f\n", (sum(Error_test_nofeatures) - sum(Error_test)) / sum(Error_test_nofeatures)))
-
-writeLines("Regularized Linear regression:")
-writeLines(paste("- Training error:", sum(Error_train_rlr) / sum(CV$TrainSize)))
-writeLines(paste("- Test error:", sum(Error_test_rlr) / sum(CV$TestSize)))
-writeLines(paste("- R^2 train: ", (sum(Error_train_nofeatures) - sum(Error_train_rlr)) / sum(Error_train_nofeatures)))
-writeLines(paste("- R^2 test:", (sum(Error_test_nofeatures) - sum(Error_test_rlr)) / sum(Error_test_nofeatures)))
 
 
-writeLines("Weights in last fold :")
-for (m in 1:M) {
-  writeLines(paste(attributeNames[m], ws_final[m, k]))
-}
 
 
